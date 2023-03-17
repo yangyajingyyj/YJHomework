67e6794e125b94cfd2a96f96bf12edbb30f89b2c
yangyajingyyj <126839944+yangyajingyyj@users.noreply.github.com>
2023-03-17 10:20:44 +0800

    Add files via upload

---
+++ homework1/counter/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace counter
+{ 
+    class counter
+    {
+        static void Main(string[] arge)
+        {
+            int num1;
+            int num2;
+            string operand;
+            ConsoleKeyInfo status;
+            float answer;
+
+            while (true)
+            {
+                Console.Write("Please enter the first integer: ");
+                num1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Please enter the second integer: ");
+                num2 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Please enter an operand (+, -, /, *): ");
+                operand = Console.ReadLine();
+
+                switch (operand)
+                {
+                    case "-":
+                        answer = num1 - num2;
+                        break;
+                    case "+":
+                        answer = num1 + num2;
+                        break;
+                    case "/":
+                        answer = num1 / num2;
+                        break;
+                    case "*":
+                        answer = num1 * num2;
+                        break;
+                    default:
+                        answer = 0;
+                        break;
+                }
+
+                Console.WriteLine(num1.ToString() + " " + operand + " " + num2.ToString() + " = " + answer.ToString());
+
+            }
+        }
+    }
+
+}
+
